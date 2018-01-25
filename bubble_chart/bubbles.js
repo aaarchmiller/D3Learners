89710010b1bfc254b0da5077ce967b9d73921f1b
Lindsay Carr <lcarr@usgs.gov>
2018-01-25 13:01:22 -0600

    restart simulation + bounce + actually disable button

--- bubble_chart/bubbles.js
+++ bubble_chart/bubbles.js
@@ -8,12 +8,15 @@ function add_bubbles() {
 			.domain([3, 30])
 			.range([15, 80]);
 
+	var strength = 10;
+	var strengthForce = d3.forceManyBody().strength(strength);
 	var simulation = d3.forceSimulation()
 		.force("center", d3.forceCenter(plotwidth / 2, plotheight / 2))
-		.force("charge", d3.forceManyBody().strength(20))
+		.force("charge", strengthForce)
 		.force('collision', d3.forceCollide().radius(function(d) {
 			return d.freq * 10;
 		}))
+		.alpha(3)
 		.on("tick", ticked);
 	
 	var bubbles = plotarea
@@ -82,12 +85,7 @@ function add_bubbles() {
 
 	function update_bubbles(data, data_name, header_name) {
 		
-		// bubbles = bubbles.data(data);
-		// bubbles.exit().remove();
-		// bubbles = bubbles.enter();
-		// setup_bubbles(data, data_name);
-		// simulation.nodes(data);
-		// simulation.restart();
+		simulation.alpha(3).restart();
 
 		d3.selectAll(".bubble circle")
 			.transition().duration(1000)
@@ -100,7 +98,14 @@ function add_bubbles() {
 
 	}
 
+	var count = 0;
 	function ticked() {
+		count++;
+
+		// switch strength between +/- for bounce effect
+		if(count%50 == 0) { strength = strength * -1; }
+		strengthForce.strength(strength);
+
 		bubbles.selectAll("circle")
 			.attr('cx', function(d) { return d.x })
 			.attr('cy', function(d) { return d.y });
@@ -145,24 +150,32 @@ function buttonLogic(counter, prev_button, adv_button) {
 	
 	// previous button
 	if (counter <= 0) {
+		// off
 		prev_button
-			.classed("enabled", false)
-			.classed("disabled", true);
+			.attr("disabled", true)
+			.classed("on", false)
+			.classed("off", true);
 	} else {
+		// on
 		prev_button
-			.classed("enabled", true)
-			.classed("disabled", false);
+			.attr("disabled", null)
+			.classed("on", true)
+			.classed("off", false);
 	}
 
 	// advance button
 	if (counter < (views.length-1)) {
+		// on
 		adv_button
-			.classed("enabled", true)
-			.classed("disabled", false);
+			.attr("disabled", null)
+			.classed("on", true)
+			.classed("off", false);
 	} else {
+		// off
 		adv_button
-			.classed("enabled", false)
-			.classed("disabled", true);
+			.attr("disabled", true)
+			.classed("on", false)
+			.classed("off", true);
 	}
 
 }
